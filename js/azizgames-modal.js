3322c9329791dadd43598a6dab1f662feceec14e
admin <admin@MacBookAirKruHambal.local>
2026-09-14 14:58:56 +0700

    chore: bump app version to 10.22.707

--- js/azizgames-modal.js
+++ js/azizgames-modal.js
@@ -1,4 +1,4 @@
-import { APP_VERSION } from './version.js?v=10.22.705'
+import { APP_VERSION } from './version.js?v=10.22.707'
 import { supabase } from './supabase.js'
 
 const AZIZGAMES_PATH = 'azizgames.html'
