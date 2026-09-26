9675780c8e587180d91bb0ae3f15e28f95d61146
admin <admin@MacBookAirKruHambal.local>
2026-09-26 16:31:04 +0700

    fix azizgames official interface parity

--- js/azizgames-bootstrap.js
+++ js/azizgames-bootstrap.js
@@ -1,6 +1,6 @@
 import { getWorkloadState } from './workload-scheduler.js'
 
-const VERSION = '10.22.810'
+const VERSION = '10.22.812'
 const base = location.pathname.startsWith('/pp5online/') ? '/pp5online/' : '/'
 
 const loadScript = (src) => new Promise((resolve, reject) => {
